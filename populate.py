82ca31cdf04b37465b4fd33c79d281eb579fba05
Shivam Kohli <kohlishivam5522@gmail.com>
2017-07-23 04:53:40 +0530

    updated populate.py

--- populate.py
+++ populate.py
@@ -13,8 +13,7 @@ from timezonefinder import TimezoneFinder
 # Array of countries along with there languages imported 
 import countryLanguage.py
 
-from messengerbot.models import user , statusCode , status , typeOfService , modeOfContact , typeOfShipment , typeOfCollection , typeOfBox , address , language , country , place , order
-
+from messengerbot.models import user , status_code , status , type_of_service , mode_of_contact , type_of_shipment , type_of_collection , type_of_box , address , language , country , place , order
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -23,7 +22,9 @@ sys.setdefaultencoding("utf-8")
 
 
 def populate_database():
-#     json of the places where DHL provides services.
+    """
+    This function is used to populate the database by iterating over the list of DHL centres in various states and cities.
+    """
     url = 'https://gist.githubusercontent.com/mayurah/1302855181e4b5e3b05211d242ae592a/raw/7034304a7efdac74011790227ca6c8aa7ca994b1/countriesToCities.json'
     response = urlopen(url)
     json_countries = json.load(response)
@@ -47,10 +48,13 @@ def populate_database():
 
 
 def timezone_of_place(place , country):
-  response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + place + ',' + country)
-  resp_json_payload = response.json()
-  latitude = (resp_json_payload['results'][0]['geometry']['location']['lat'])
-  longitude = (resp_json_payload['results'][0]['geometry']['location']['lng'])
+    """
+    This function uses google api to find the timezone of various countries.
+    """
+    response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + place + ',' + country)
+    resp_json_payload = response.json()
+    latitude = (resp_json_payload['results'][0]['geometry']['location']['lat'])
+    longitude = (resp_json_payload['results'][0]['geometry']['location']['lng'])
 
 
   tf = TimezoneFinder()
@@ -58,6 +62,9 @@ def timezone_of_place(place , country):
 
 
 def language(country):
+    """
+    This function provies a array of languages spoken in a particular country
+    """
     array_languages = []
     for i in country_language:
         print i['country']
